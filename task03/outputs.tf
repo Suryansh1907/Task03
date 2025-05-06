b83bd431fcd627c4240fcae2cee68a7f14a65fb5
Suryansh1907 <suryansh_tilak@epam.com>
2025-05-06 23:54:25 +0530

    Update outputs.tf

--- task03/outputs.tf
+++ task03/outputs.tf
@@ -1,11 +1,14 @@
 output "rg_id" {
-  value = azurerm_resource_group.rg.id
+  description = "The ID of the Azure Resource Group"
+  value       = azurerm_resource_group.rg.id
 }
 
 output "sa_blob_endpoint" {
-  value = azurerm_storage_account.sa.primary_blob_endpoint
+  description = "The primary endpoint of the blob service for the Storage Account"
+  value       = azurerm_storage_account.sa.primary_blob_endpoint
 }
 
 output "vnet_id" {
-  value = azurerm_virtual_network.vnet.id
+  description = "The ID of the Azure Virtual Network"
+  value       = azurerm_virtual_network.vnet.id
 }
