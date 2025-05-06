030f926cea63caab8805d78c3c2451379e286d68
Suryansh1907 <suryansh_tilak@epam.com>
2025-05-07 01:07:04 +0530

    Update main.tf

--- task03/main.tf
+++ task03/main.tf
@@ -1,21 +1,21 @@
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
+  name     = var.rg_name
   location = var.location
 
   tags = {
-    Creator = var.creator
+    Creator = var.student_email
   }
 }
 
 resource "azurerm_storage_account" "sa" {
-  name                     = var.storage_account_name
+  name                     = var.storageaccount_name
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
   tags = {
-    Creator = var.creator
+    Creator = var.student_email
   }
 }
 
@@ -26,19 +26,19 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
 
   tags = {
-    Creator = var.creator
+    Creator = var.student_email
   }
 }
 
-resource "azurerm_subnet" "frontend" {
-  name                 = "frontend"
+resource "azurerm_subnet" "subnet1" {
+  name                 = var.subnet1_name
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_subnet" "backend" {
-  name                 = "backend"
+resource "azurerm_subnet" "subnet2" {
+  name                 = var.subnet2_name
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
